a17f522352974f5f9d7c2a946028aa06707e019b
kiffness <ktkage80uk@gmail.com>
2019-05-13 11:04:33 +0100

    Removed Computers.ps1 replaced with computer_name

--- PowerShell/Computers.ps1
+++
@@ -1,3 +0,0 @@
-﻿Import-Module activedirectory
-
-Get-ADComputer -Filter * -Properties * | Select-Object Name,OperatingSystem,LastLogonDate | Export-Csv F:\Windows_10_Refresh\Powershell\All-Windows.csv -NoTypeInformation -Encoding UTF8
---
+++ PowerShell/computer_name_only.ps1
@@ -0,0 +1,9 @@
+﻿Import-Module activedirectory
+
+Get-ADComputer -Filter * | Select-Object -ExpandProperty Name | Sort-Object | Out-File AllComputerName.txt -Encoding utf8
+
+$MyPath = "$env:F:\Windows_10_Refresh\Powershell\AllComputerName.txt" # read the file in
+$MyFile = Get-Content $MyPath
+$Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
+$MyPathOut = "F:\Windows_10_Refresh\Powershell\NoBOM.txt" # export it here without BOM
+[System.IO.File]::WriteAllLines($MyPathOut, $MyFile, $Utf8NoBomEncoding)
