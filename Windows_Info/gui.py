9706a10965f5c62218c20c2fec588bce7c3d34d4
kiffness <ktkage80uk@gmail.com>
2019-05-29 15:38:29 +0100

    Changed Layout of GUI

--- Windows_Info/gui.py
+++ Windows_Info/gui.py
@@ -2,98 +2,64 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import Menu
 
-large_font = ("Verdana", 16)
+import misc_func
 
-def hello():
-    print("Hello")
+LARGE_FONT= ("Verdana", 12)
 
 
-class QueryMenu(ttk.Frame):
-    def __init__(self, parent, controller):
-        super().__init__(self, parent)
+class AllWidgets(tk.Frame):
+    """A class for all my widgets"""
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
 
-        label = ttk.Label(self, text="Query Menu")
-        label.grid(row=0)
 
-        main_button = ttk.Button(self, text="Main Menu",
-                                 command=lambda: controller.show_frame(MainMenu))
-        main.button.grid(column=0, row=1)
+        # Script Menu section 
+        script_menu = tk.LabelFrame(self, text="PowerShell",
+                                    font=("TkDefualtFont", 14))
+        description_label = tk.Label(script_menu, text="Button Descriptions\nRefresh runs a script to refresh all the computer names\n" + 
+                                     "Add runs the script that gets the info about a computer and the adds it to the database")
 
-        # TODO: add more buttons for the various script
+        # All Buttons for Powershell menu
+        refresh_bt = ttk.Button(script_menu,
+                                text="Refresh",
+                                command=misc_func.refresh_list).grid(row=1, column=1)
+        add_bt = ttk.Button(script_menu,
+                            text="Add",
+                            command=misc_func.powershell).grid(row=1, column=3)
 
-class ScriptMenu(ttk.Frame):
-    def __init__(self, parent, controller):
-        super().__init__(self, parent)
-        label = ttk.Label(self, text="Script Menu")
-        label.grid(row=0)
+        
 
-        main_button = ttk.Button(self, text="Main Menu",
-                                 command=lambda: controller.show_frame(MainMenu))
-        main.button.grid(column=0, row=1)
+        script_menu.grid(row=0, column=0, sticky=tk.W + tk.E)
+        description_label.grid(row=0, columnspan=5)
 
-        # TODO: add more buttons for the various scripts
-
-class MainMenu(ttk.Frame):
-    def __init__(self, parent, controller):
-        super().__init__(self, parent)
-
-        label = ttk.Label(self, text="Main Menu", font=large_font)
-        label.grid(row=0)
-
-        script_button = ttk.Button(self, text="Scripts",
-                                   command=lambda: controller.show_frame(ScriptMenu))
-        script_button.grid(column=0, row=1)
-
-        query_button = ttk.Button(self, text="Queries",
-                                  command=lambda: controller.show_frame(QueryMenu))
-        query_button.grid(column=1, row=1)
 
 class MenuBar(Menu):
     """The MenuBar for our root Window"""
     def __init__(self, parent, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
 
-        fileMenu = Menu(self, tearoff=False)
-        self.add_cascade(label="File",underline=0, menu=fileMenu)
-        fileMenu.add_command(label="Exit", underline=1, command=self.quit)
-
-
 class Application(tk.Tk):
-    """Main Window"""
-    
+
     def __init__(self, *args, **kwargs):
+        
         super().__init__(*args, **kwargs)
 
         self.title("Windows Refresh Application")
-
-        # Uncomment to not allow resizing
+        
+        # Uncomment to dis-allow resizing
         # self.resizable(width=False, height=False)
-
-        menubar = MenuBar(self)
-        self.config(menu=menubar)
 
         ttk.Label(
             self,
             text="Windows Refresh Application",
             font=("TkDefualtFont", 16)
-            ).grid(row=1)
+            ).grid(row=0, columnspan=5, padx=5, pady=5)
 
-        self.frames = {}
+        self.widgets = AllWidgets(self)
+        self.widgets.grid(row=1)
 
-        container = ttk.Frame(self)
-        container.grid(row=3, padx=10)
-
-        for f in (MainMenu, ScriptMenu, QueryMenu):
-            frame = f(container, self)
-            self.frames[f] = frame
-            frame.grid(row=4)
-
-        self.show_frame(MainMenu)
         
-    def show_frame(self, cont):
-        frame = self.frames[cont]
-        frame.tkraise()
-
+        
 
 def main():
     app = Application()
