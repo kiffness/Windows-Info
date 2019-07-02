b8776fcd0b7edc5b98b07860f0977f7a51de3e3c
kiffness <ktkage80uk@gmail.com>
2019-07-02 10:21:28 +0100

    moved text menu from ui.py

---
+++ Windows_Info/menu.py
@@ -0,0 +1,8 @@
+def display_select_os_menu():
+    """Prints the Menu for db.select_os"""
+    print("OS Choice(Enter Number)")
+    print(" ")
+    print("-" * 95)
+    print("1.Windows 7 SP1")
+    print("2.Windows 10")
+    print("-" * 95)
