2501f8ca0fa5a436b7f650ddbeb251d16ef8e4d1
kiffness <ktkage80uk@gmail.com>
2019-07-04 11:39:03 +0100

    removed menu functions from ui.py

--- Windows_Info/menu.py
+++ Windows_Info/menu.py
@@ -6,3 +6,35 @@ def display_select_os_menu():
     print("1.Windows 7 SP1")
     print("2.Windows 10")
     print("-" * 95)
+
+def main_menu():
+    """Print the main menu and options"""
+    print("WELCOME TO WINDOWS 10 REFRESH PROGRAM!")
+    print(" ")
+    print("-" * 95)
+    print("script - Powershell and Python functions")
+    print("query - Database queries")
+    print("exit - Exit the application")
+    print("-" * 95)
+
+def display_menu():
+    """Prints the Script menu and options"""
+    print("Windows 10 Refresh Application")
+    print(" ")
+    print("COMMAND MENU")
+    print("-" * 95)
+    print("Main - Adds and Updates data in database")
+    print("refresh - Refresh Computer names for add")
+    print("read - Read text file")
+    print("back - back to main menu")
+    print("-" * 95)
+
+def display_query_menu():
+    """Prints Query menu and options"""
+    print("QUERY MENU")
+    print(" ")
+    print("-" * 95)
+    print("all - Shows all info")
+    print("os - Select an os and show all pc's")
+    print("back - Back to main menu")
+    print("-" * 95)
--- Windows_Info/ui.py
+++ Windows_Info/ui.py
@@ -1,48 +1,17 @@
 import db
 import misc_func
+import menu
 from objects import Computer
 
-def main_menu():
-    """Print the main menu and options"""
-    print("WELCOME TO WINDOWS 10 REFRESH PROGRAM!")
-    print(" ")
-    print("-" * 95)
-    print("script - Powershell and Python functions")
-    print("query - Database queries")
-    print("exit - Exit the application")
-    print("-" * 95)
-
-def display_menu():
-    """Prints the Script menu and options"""
-    print("Windows 10 Refresh Application")
-    print(" ")
-    print("COMMAND MENU")
-    print("-" * 95)
-    print("Main - Adds and Updates data in database")
-    print("refresh - Refresh Computer names for add")
-    print("read - Read text file")
-    print("back - back to main menu")
-    print("-" * 95)
-
-def display_query_menu():
-    """Prints Query menu and options"""
-    print("QUERY MENU")
-    print(" ")
-    print("-" * 95)
-    print("all - Shows all info")
-    print("os - Select an os and show all pc's")
-    print("back - Back to main menu")
-    print("-" * 95)
-    
 
 def main():
     """Main Function Refrences all other functions and accepts user input"""
     db.connect()
-    main_menu()
+    menu.main_menu()
     while True:
         command_main = input("Please choose a menu: ").rstrip()
         if command_main == "script":
-            display_menu()
+            menu.display_menu()
             while True:
                 command_script = input("Enter Command: ").rstrip()
                 if command_script == "main":
@@ -54,11 +23,11 @@ def main():
                 elif command_script == "test":
                    misc_func.last_logon()
                 elif command_script == "back":
-                    main_menu()
+                    menu.main_menu()
                     break
                 continue
         elif command_main == "query":
-            display_query_menu()
+            menu.display_query_menu()
             while True:
                 command_query = input("Choose a query: ").rstrip()
                 if command_query == "all":
@@ -66,14 +35,14 @@ def main():
                 elif command_query == "os":
                     misc_func.format_select_os()
                 elif command_query == "back":
-                    main_menu()
+                    menu.main_menu()
                     break
                 continue
         elif command_main == "exit":
             break
         else:
             print("Not a Valid Command")
-            main_menu()
+            menu.main_menu()
     db.close()
     print("The application has closed succesfully")
     
